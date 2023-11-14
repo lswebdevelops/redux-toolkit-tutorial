0dbc5e6e8ebf0379a6c743d327c9d52011f16f65
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-11-14 14:57:19 -0300

    creating simple counter using redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import React, { useState } from "react";
-
-import "./styles.css";
-
-export default function App() {
-
-  const [count , setCount ] = useState(0);
+import Counter from "./features/counter/Counter";
 
 
-
+function App() {
   return (
-    <div className="App">
-      <h1>The count is : {count}</h1>
-      <button onClick={() => setCount(count -1)}>Decrement by 1</button>
-      <button onClick={() => setCount(count + 1)}>Increment by 1</button>
-      <button onClick={() => setCount(count + 33)}>Increment by 33</button>
-      <button onClick={() => setCount(count - 33)}>Decrement by 33</button>
-    </div>
+    <main className="App">
+   <Counter />
+    </main>
   );
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";  // Import createRoot from "react-dom/client"
 import App from "./App";
+import { store } from './app/store'
+import { Provider } from "react-redux";
 
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <StrictMode>   
-      <App />   
-  </StrictMode>,
-  rootElement
+const root = createRoot(rootElement);  // Use createRoot from "react-dom/client"
+
+root.render(
+  <StrictMode>
+   
+  <Provider store={store}>
+  <App />
+  </Provider>
+ 
+  </StrictMode>
 );
