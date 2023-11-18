b1f29889d78b36714d7d3ba1f069ede8990f28ef
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-11-17 22:11:21 -0300

    adding user friendly date from date-fns and show newest first

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,17 +1,29 @@
 import { useSelector } from "react-redux";
 import { selectAllPosts } from "./postsSlice";
-import React from 'react'
+import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
 
 const PostsList = () => {
 
      
     const posts = useSelector(selectAllPosts)
 
-    const renderedPosts = posts.map(post => (
+    // show the newest first 
+    
+    const orderedPosts = posts.slice().sort((a,b) => b.date.localeCompare(a.date));
+
+
+
+    const renderedPosts = orderedPosts.map(post => (
         <article key={post.id}>
             <h3>{post.title}</h3>
             {/* substring> previews the first 100 caracters */}
             <p>{post.content.substring(0,100)}</p>
+
+            <p className="postCredit">
+                <PostAuthor userId={post.userId} />
+                <TimeAgo timestamp={post.date} />
+            </p>
         </article>
     ))
 
